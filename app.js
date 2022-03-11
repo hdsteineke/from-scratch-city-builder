ad0f74fad9aba3ee790c8c6a42584bde2813feb9
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-03-10 20:52:02 -0800

    set event listener for castle image and successfully rotating through castle images

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // import functions and grab DOM elements
-const castleDrop = document.getElementsById('castle-dropdown');
-const waterDrop = document.getElementsById('water-dropdown');
-const skyDrop = document.getElementsById('sky-dropdown');
+const castleDrop = document.getElementById('castle-dropdown');
+const waterDrop = document.getElementById('water-dropdown');
+const skyDrop = document.getElementById('sky-dropdown');
 
 const castleImg = document.getElementById('castle-img');
 const waterImg = document.getElementById('water-img');
@@ -19,6 +19,23 @@ let skyCount = 0;
 let slogans = [];
 
 // set event listeners 
+castleDrop.addEventListener('change', () => {
+  // get the value of the head dropdown
+    const castleType = castleDrop.value;
+
+  // increment the head change count state
+    castleCount++;
+  
+  // update the dom for the head (use style.backgroundImage on the bottomEl div instead of trying to set the .src -- it's NOT an img tag!)
+    //headEl.style.backgroundImage = `url("./assets/${headType}-head.png")`;
+  
+    castleImg.src = `./assets/${castleType}.png`;
+
+  // update the stats to show the new count (call displayStats() to do this work)
+
+});
+
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
