cc45364fd80b48d1de5628ef5fb4c7c80b508d77
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-03-11 09:41:31 -0800

    created working eventListener for slogan input, and now I need to clear array with each input

--- app.js
+++ app.js
@@ -6,21 +6,18 @@ const skyDrop = document.getElementById('sky-dropdown');
 const castleImg = document.getElementById('castle-img');
 const waterImg = document.getElementById('water-img');
 const skyImg = document.getElementById('sky-img');
-/* 
+
 const sloganInput = document.getElementById('slogan-input');
 const sloganButton = document.getElementById('slogan-button');
-
-Commenting out my unused variables for now to pass CI until I complete my assignment
-
 const sloganDisplay = document.getElementById('show-slogans');
 
 // let state
 let castleCount = 0;
 let waterCount = 0;
 let skyCount = 0;
-*/
 
-/* let slogans = [];
+
+let slogans = [];
 
 // set event listeners 
 castleDrop.addEventListener('change', () => {
@@ -73,16 +70,32 @@ skyDrop.addEventListener('change', () => {
 
 });
 
-/* sloganButton.addEventListener('change', () => {
-  // get the value of the head dropdown
+
+sloganInput.addEventListener('input', () => {
+
     const newSlogan = sloganInput.value;
 
     slogans.push(newSlogan);
 
-    sloganInput.value = '';
-  
+
+    console.log(newSlogan);
+
 
 
 });
 
-*/
+
+
+sloganButton.addEventListener('click', () => {
+
+    const newSlogan = sloganInput.value;
+
+    slogans.push(newSlogan);
+
+    sloganInput.value = ' ';
+
+    console.log('click');
+  
+
+
+});
